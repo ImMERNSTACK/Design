cba0be04221a62fade5f757c546bcd2dfd7fc09c
ImMERNSTACK <bhartiaman665@gmail.com>
2025-01-09 11:53:51 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
-
-import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
-import Header from './Component/Header';
-
 
 function App() {
   return (
     <div>
-    <Routes>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/practice" element={<Header/>}/>
-    </Routes>
+      <Home/>
   </div>
   );
 }
